55e5f8dd206d7cfd13205f41940f18e4ad1e8693
[aviv polak] <[avivp0437@gmail.com]>
2021-11-11 08:35:16 +0200

    add morgan (tiny+body)

--- index.js
+++ index.js
@@ -21,6 +21,23 @@ mongoose
     .catch((error) => {
         console.log("error connecting to MongoDB:", error.message);
     });
+morgan.token("body", function (req, res) {
+    return JSON.stringify(req.body);
+});
+app.use(
+    morgan(function (tokens, req, res) {
+        return [
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, "content-length"),
+            "-",
+            tokens["response-time"](req, res),
+            "ms",
+            tokens.body(req, res),
+        ].join(" ");
+    })
+);
 
 app.get("/", (req, res) => {
     res.send("working");
